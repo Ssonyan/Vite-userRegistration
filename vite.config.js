12f8dd9d7210d1c9a68853eb5ff171284cc08947
Ssonyan <ssonyan@live.fr>
2024-11-18 19:16:24 +0100

    deploy wip

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+    base: '/Vite-userRegistration/',
     plugins: [react()],
     test: {
         environment: 'jsdom',
